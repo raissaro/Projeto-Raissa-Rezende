6e556747a6faffb6aeb4fec57f4d2cd2a0634acd
raissa <78005805+raissaro@users.noreply.github.com>
2022-12-02 23:53:15 -0300

    Classe A e Classe B

---
+++ A.java
@@ -0,0 +1,14 @@
+public class A{
+    private int A1;
+    private float A2;
+
+    public int getA1(){
+        return A1;
+    }
+
+    public float getA2(){
+        return A2;
+    }
+
+    pu
+}
---
+++ B.java
@@ -0,0 +1,4 @@
+public class B{
+    private int B1;
+    private float B2; 
+}
