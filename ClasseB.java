4ba8e3f5d9fe250ceea9cca8d1b39555e2f4d7d6
raissa <78005805+raissaro@users.noreply.github.com>
2022-12-03 14:32:08 -0300

    Add files via upload
    Classe B

---
+++ ClasseB.java
@@ -0,0 +1,28 @@
+public class ClasseB{
+    private int B1;
+    private float B2; 
+
+    public int getA1(){
+        return B1;
+    }
+
+    public float getA2(){
+        return B2;
+    }
+ 
+    public void setA1(int B1){
+        this.B1 = B1;
+    }
+
+    public void setA2(float B2){
+        this.B2 = B2;
+    }
+
+    public void MB1(){
+        System.out.println("MB1");
+    }
+
+    public void MB2(){
+        System.out.println("MB2");
+    }
+}
